0c181c2d2ca83a238017e764dce6f90afe49c4af
laura98dhz <laura98dhz@gmail.com>
2022-05-06 17:53:23 +0200

    Fichero .env y fichero config

--- backend/usuarios/usuarios.repository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import { UsuariosEntity } from "./usuarios.entity";
-
-@EntityRepository(UsuariosEntity) 
-export class UsuariosRepository extends Repository<UsuariosEntity> {}
