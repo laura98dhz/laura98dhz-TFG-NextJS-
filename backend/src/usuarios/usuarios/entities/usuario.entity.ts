22ee6b03143c10bcdeb89b4696991f8014d062f7
Laura de Hoyos Zurdo <laura.hoyos@optimissa.com>
2022-05-10 16:36:36 +0200

    agregar a usuario.entity tipo de columna
    falta la relacion con la otra tabla

--- backend/src/usuarios/usuarios/entities/usuario.entity.ts
+++ backend/src/usuarios/usuarios/entities/usuario.entity.ts
@@ -1,5 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
 export class UsuarioEntity{
+    
+    @PrimaryGeneratedColumn()
     readonly nombreUsuario: string; 
+
+    @Column()
     readonly contraseña: string;
+    
+    @Column()
     readonly correo: string;
 }
+
+//https://typeorm.io/relations
