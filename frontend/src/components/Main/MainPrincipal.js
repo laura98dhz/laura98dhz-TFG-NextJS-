c93996ad55d9832f17ad8e6fc4c44f9b22c96979
Laura de Hoyos Zurdo <laura.hoyos@optimissa.com>
2022-05-19 10:46:49 +0200

    detalles botones

--- frontend/src/components/Main/MainPrincipal.js
+++ frontend/src/components/Main/MainPrincipal.js
@@ -1,13 +1,33 @@
 import logo from '../../Recursos/img/logo2.png';
 import CalculadoraHipoteca from '../CalculadoraHipoteca/CalculadoraHipoteca';
 import { useRef } from 'react';
+
 function MainPrincipal(props){
 
     const buscar = useRef(null);
 
+    const alquiler = useRef(null);
+    const comprar = useRef(null);
+
     function selectOpcionTransaccion(e, opcion){
         e.nativeEvent.preventDefault();
-        props.opcionOnClick(opcion)
+        props.opcionOnClick(opcion);
+
+        if(opcion === "Venta"){
+
+            comprar.current.style.backgroundColor="rgb(176, 158, 128)";
+            comprar.current.style.color="white";
+
+            alquiler.current.style.backgroundColor="white";
+            alquiler.current.style.color="rgb(70, 70, 70)";
+
+        }else{
+            alquiler.current.style.backgroundColor="rgb(176, 158, 128)";
+            alquiler.current.style.color="white";
+
+            comprar.current.style.backgroundColor="white";
+            comprar.current.style.color="rgb(70, 70, 70)";
+        }
     }
     
     function buscarInmueble(e, ubicacion){
@@ -28,8 +48,8 @@ function MainPrincipal(props){
                         <h2 className='main-principal-container-buscar-titulo'>Encuentre la casa de sus sueños</h2>
                         <form className='main-principal-container-buscar-form'>
                             <div className='main-principal-container-buscar-form-botones'>
-                                <button className='main-principal-container-buscar-form-botones--1' onClick={(e)=>selectOpcionTransaccion(e, "Venta")}>Comprar</button>
-                                <button className='main-principal-container-buscar-form-botones--2' onClick={(e)=>selectOpcionTransaccion(e, "Alquiler")}>Alquilar</button>
+                                <button ref={comprar} className='main-principal-container-buscar-form-botones--1' value="Venta" onClick={(e)=>selectOpcionTransaccion(e, "Venta")}>Comprar</button>
+                                <button ref={alquiler} className='main-principal-container-buscar-form-botones--2' value="Alquiler" onClick={(e)=>selectOpcionTransaccion(e, "Alquiler")}>Alquilar</button>
                             </div>
                             <input ref={buscar} type='text' className='main-principal-container-buscar-form-text'></input>
                             <button className='main-principal-container-buscar-form-text-boton' onClick={(e)=>buscarInmueble(e, buscar)}>
